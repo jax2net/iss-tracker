7028f923772c778a26aeb9606de3c5cf076f00b3
Jacob Santos <jsns@disroot.org>
2020-07-19 17:14:55 -0700

    Updates

---
+++ precache-manifest.ffc3ef98d292845141bc56a4a338c576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d015ebc5ca9fdadb2c6dd92c5c98dfba",
+    "url": "/iss-tracker/index.html"
+  },
+  {
+    "revision": "fe5734fc531d4f30e4a3",
+    "url": "/iss-tracker/static/css/main.4fc567b5.chunk.css"
+  },
+  {
+    "revision": "2bacdff18a68ac69baf2",
+    "url": "/iss-tracker/static/js/2.81bced08.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/iss-tracker/static/js/2.81bced08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5734fc531d4f30e4a3",
+    "url": "/iss-tracker/static/js/main.159e4d17.chunk.js"
+  },
+  {
+    "revision": "21177966f3aec9b618d2",
+    "url": "/iss-tracker/static/js/runtime-main.6fe19a8d.js"
+  }
+]);
